429018816344e27cf8ace90a88bf478d4b525c0c
Paul Shen <47463128+GZeppelin@users.noreply.github.com>
2023-07-21 11:18:50 -0700

    Abstract axis input

--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -9,12 +9,35 @@ public enum Buttons
     Top, 
     Bottom
 }
+public enum Condition
+{
+    GreaterThan,
+    LessThan
+}
 [System.Serializable]
 public class InputAxisState
 {
     public string axisName;
     public float offvlue;
     public Buttons button;
+    public Condition condition;
+
+    public bool value
+    {
+        get
+        {
+            var val = Input.GetAxis(axisName); 
+            switch(condition)
+            {
+                case Condition.GreaterThan:
+                    return val > offvlue;
+                case Condition.LessThan: 
+                    return val < offvlue;
+            
+            }
+            return false;
+        }
+    }
 }
 public class InputManager : MonoBehaviour
 {
@@ -28,6 +51,12 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (var input in inputs)
+        {
+            if (input.value)
+            {
+                Debug.Log("Input detected " + input.button);
+            }
+        }
     }
 }
